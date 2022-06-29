a3338d0273d1f518a27ac5b5c369407192d2506c
Kenan <kenanwright@gmail.com>
2022-06-29 11:19:04 +0200

    Add Nav component and static props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
-        <h2>Hi</h2>
+        <Navbar />
       </div>
-  );
-}
+    );
+  }
 }
 
 export default App;
