1296ec836f720dd315555a78c1e1fc2808a62dc1
Kenan <kenanwright@gmail.com>
2022-07-03 11:22:46 +0200

    Add links to about and home- not working fully

--- src/App.js
+++ src/App.js
@@ -1,56 +1,70 @@
-import React, { Component } from "react";
-import Navbar from "./components/layout/Navbar";
-import Users from "./components/users/Users";
-import Search from "./components/users/Search";
-import "./App.css";
-import axios from "axios";
-import Alert from "./components/layout/Alert";
+import React, { Component, Fragment } from 'react'
+import axios from 'axios'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import Navbar from './components/layout/Navbar'
+import Users from './components/users/Users'
+import Search from './components/users/Search'
+import Alert from './components/layout/Alert'
+import About from './components/pages/About'
+import './App.css'
 
 class App extends Component {
   state = {
     users: [],
     loading: false,
     alert: null
-  };
+  }
 
-  // Search Github users with state text
-  searchUsers = async (text) => {
-    this.setState({ loading: true });
+  // search github users
+  searchUsers = async text => {
+    this.setState({ loading: true })
     const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    )
+    this.setState({ users: res.data.items, loading: false })
+  }
 
-    this.setState({ users: res.data.items, loading: false });
-  };
+  // clear users
+  clearUsers = () => this.setState({ users: [], loading: false })
 
-  //clear users from state
-  clearUsers = () => this.setState({ users: [], loading: false });
-
-  // Set Alert 
+  // set Alert
   setAlert = (msg, type) => {
-    this.setState({ alert: {msg: msg, type: type }});
-    setTimeout(() => this.setState({alert: null}), 5000);
+    this.setState({ alert: { msg, type } })
+    setTimeout(() => this.setState({ alert: null }), 5000)
   }
 
   render() {
-    const { users, loading } = this.state;
-
+    const { users, loading } = this.state
     return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Alert alert={this.state.alert} />
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-          />
-          <Users loading={loading} users={users} />
+      <Router>
+        <div className='App'>
+          <Navbar />
+          <div className='container'>
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path='/'
+                render={props => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
         </div>
-      </div>
-    );
+      </Router>
+    )
   }
 }
 
-export default App;
+export default App
