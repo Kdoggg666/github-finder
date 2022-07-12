8e985e47248ac543a9b0208ad86e82bc4a962c78
Kenan <kenanwright@gmail.com>
2022-07-12 17:43:13 +0200

    Refactor Users and User

--- src/App.js
+++ src/App.js
@@ -8,38 +8,13 @@ import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
 import User from "./components/users/User";
+import GithubState from "./context/github/GithubState";
 import "./App.css";
 
 const App = () => {
-  const [users, setUsers] = useState([]);
-  const [user, setUser] = useState({});
   const [repos, setRepos] = useState([]);
   const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
-
-
-  // search github users
-  const searchUsers = async (text) => {
-    setLoading(true);
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-   setUsers(res.data.items);
-   setLoading(false);
-  };
-
-  // get single github user
-  const getUser = async (username) => {
-    setLoading(true);
-
-    console.log(username);
-    const res = await axios.get(
-      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-    
-    setUser(res.data);
-    setLoading(false);
-  };
 
   // Get users repos
   const getUserRepos = async (username) => {
@@ -49,16 +24,10 @@ const App = () => {
     const res = await axios.get(
       `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
-    
+
     setRepos(res.data);
     setLoading(false);
   };
-
-  // clear users
-  const clearUsers = () => {
-    setUsers([]);
-    setLoading(false);
-  }
 
   // set Alert
   const showAlert = (msg, type) => {
@@ -66,8 +35,8 @@ const App = () => {
     setTimeout(() => setAlert(null), 5000);
   };
 
-
-    return (
+  return (
+    <GithubState>
       <Router>
         <div className="App">
           <Navbar />
@@ -79,13 +48,8 @@ const App = () => {
                 path="/"
                 render={(props) => (
                   <Fragment>
-                    <Search
-                      searchUsers={searchUsers}
-                      clearUsers={clearUsers}
-                      showClear={users.length > 0}
-                      setAlert={showAlert}
-                    />
-                    <Users loading={loading} users={users} />
+                    <Search setAlert={showAlert} />
+                    <Users />
                   </Fragment>
                 )}
               />
@@ -94,22 +58,15 @@ const App = () => {
                 exact
                 path={`/user/:login`}
                 render={(props) => (
-                  <User
-                    {...props}
-                    getUser={getUser}
-                    getUserRepos={getUserRepos}
-                    user={user}
-                    repos={repos}
-                    loading={loading}
-                  />
+                  <User {...props} getUserRepos={getUserRepos} repos={repos} />
                 )}
               />
             </Switch>
           </div>
         </div>
       </Router>
-    );
-
-}
+    </GithubState>
+  );
+};
 
 export default App;
