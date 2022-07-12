7a5835bcb7d66349603aba49f9c95f4fdd72e191
Kenan <kenanwright@gmail.com>
2022-07-12 15:04:34 +0200

    Refactor Search.js

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,17 +1,21 @@
 import React, { Component, Fragment } from "react";
 import { Link } from "react-router-dom";
+import Repos from "../repos/Repos";
 import Spinner from "../layout/Spinner";
 import PropTypes from "prop-types";
 
 export class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
+    this.props.getUserRepos(this.props.match.params.login);
   }
 
   static propTypes = {
     loading: PropTypes.bool,
     user: PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired,
     getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired,
   };
 
   render() {
@@ -31,7 +35,7 @@ export class User extends Component {
       company,
     } = this.props.user;
 
-    const { loading } = this.props;
+    const { loading, repos } = this.props;
 
     if (loading) return <Spinner />;
 
@@ -64,7 +68,12 @@ export class User extends Component {
                 <p>{bio}</p>
               </Fragment>
             )}
-            <a href={html_url} target="_blank" className="btn btn-dark my-1">
+            <a
+              href={html_url}
+              rel="noreferrer"
+              target="_blank"
+              className="btn btn-dark my-1"
+            >
               Visit Github Profile
             </a>
             <ul>
@@ -92,13 +101,13 @@ export class User extends Component {
             </ul>
           </div>
         </div>
-
         <div className="card text-center">
-            <div className="badge badge-primary">Followers: {followers}</div>
-            <div className="badge badge-success">Following: {following}</div>
-            <div className="badge badge-light">Public Repos: {public_repos}</div>
-            <div className="badge badge-dark">Public Gists: {public_gists}</div>
+          <div className="badge badge-primary">Followers: {followers}</div>
+          <div className="badge badge-success">Following: {following}</div>
+          <div className="badge badge-light">Public Repos: {public_repos}</div>
+          <div className="badge badge-dark">Public Gists: {public_gists}</div>
         </div>
+        <Repos repos={repos} />
       </Fragment>
     );
   }
